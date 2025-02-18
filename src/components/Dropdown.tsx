ec1a39bb2b80b73b80d1e7ef4a7ac77ce6b653b8
jiwkovva19 <yankowadelyana@gmail.com>
2025-02-18 19:48:43 +0200

    added a reusable dropdown component

---
+++ src/components/Dropdown.tsx
@@ -0,0 +1,19 @@
+type DropdownProps ={
+    arr:string[];
+    defaultValue:string;
+}
+export default function Dropdown({arr,defaultValue}:DropdownProps){
+    if (!Array.isArray(arr)) {
+        console.error("Dropdown received an invalid array:", arr);
+        return null; // Prevents rendering if arr is not an array
+      }
+    return(
+
+        <select className="border-[1px] border-gray-200 rounded-md text-[23px] w-[450px] px-[5px] py-[15px]">
+            <option value=""  >{defaultValue}</option>
+              {arr.map((item, index) => (
+                <option key={index} value={item}>{item}</option>
+              ))}
+            </select>
+    )
+}
