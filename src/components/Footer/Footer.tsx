1d35b1bf37654582030d07a64db5e72bf6e702b2
MSDoshev <mihaildo@outlook.com>
2025-02-24 18:59:31 +0200

    Footer update

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -7,51 +7,51 @@ export default function Footer() {
         <div className="flex flex-col gap-2">
           <h3 className="font-bold">For TeachMate</h3>
           <ul>
-            <Link to="#">What is TeachMate?</Link>
-            <Link to="#">Become a Teacher</Link>
-            <Link to="#">FAQ</Link>
-            <Link to="#">Terms of use</Link>
-            <Link to="#">Contacts</Link>
+            <li><Link to="#">What is TeachMate?</Link></li>
+            <li><Link to="#">Become a Teacher</Link></li>
+            <li><Link to="#">FAQ</Link></li>
+            <li><Link to="#">Terms of use</Link></li>
+            <li><Link to="#">Contacts</Link></li>
           </ul>
         </div>
         <div className="flex flex-col gap-2">
           <h3 className="font-bold">Subjects</h3>
           <ul>
-            <Link to="#">Mathematics</Link>
-            <Link to="#">Bulgarian language</Link>
-            <Link to="#">English language</Link>
-            <Link to="#">German language</Link>
-            <Link to="#">French language</Link>
-            <Link to="#">Spanish language</Link>
-            <Link to="#">History</Link>
-            <Link to="#">Geography</Link>
-            <Link to="#">Biology</Link>
-            <Link to="#">Chemistry</Link>
-            <Link to="#">Physics</Link>
+            <li><Link to="#">Mathematics</Link></li>
+            <li><Link to="#">Bulgarian language</Link></li>
+            <li><Link to="#">English language</Link></li>
+            <li><Link to="#">German language</Link></li>
+            <li><Link to="#">French language</Link></li>
+            <li><Link to="#">Spanish language</Link></li>
+            <li><Link to="#">History</Link></li>
+            <li><Link to="#">Geography</Link></li>
+            <li><Link to="#">Biology</Link></li>
+            <li><Link to="#">Chemistry</Link></li>
+            <li><Link to="#">Physics</Link></li>
           </ul>
         </div>
         <div className="flex flex-col gap-2">
           <h3 className="font-bold">Cities</h3>
           <ul>
-            <Link to="#">Plovdiv</Link>
-            <Link to="#">Sofia</Link>
-            <Link to="#">Varna</Link>
-            <Link to="#">Asenovgrad</Link>
-            <Link to="#">Blagoevgrad</Link>
-            <Link to="#">Burgas</Link>
-            <Link to="#">Veliko Tarnovo</Link>
-            <Link to="#">Dobrich</Link>
-            <Link to="#">Kardzhali</Link>
-            <Link to="#">Pazardzhik</Link>
-            <Link to="#">All cities</Link>
+            <li><Link to="#">Plovdiv</Link></li>
+            <li><Link to="#">Sofia</Link></li>
+            <li><Link to="#">Varna</Link></li>
+            <li><Link to="#">Asenovgrad</Link></li>
+            <li><Link to="#">Blagoevgrad</Link></li>
+            <li><Link to="#">Burgas</Link></li>
+            <li><Link to="#">Veliko Tarnovo</Link></li>
+            <li><Link to="#">Dobrich</Link></li>
+            <li><Link to="#">Kardzhali</Link></li>
+            <li><Link to="#">Pazardzhik</Link></li>
+            <li><Link to="#">All cities</Link> </li>
           </ul>
         </div>
         <div className="flex flex-col gap-2">
           <h3 className="font-bold">Contact Info</h3>
           <ul>
-            <Link to="#">Address: #123 Some Street, Somewhere, 12345</Link>
-            <Link to="#">Email: somemail@mail.com</Link>
-            <Link to="#">Phone: +1234567890</Link>
+            <li><Link to="#">Address: #123 Some Street, Somewhere, 12345</Link></li>
+            <li><Link to="#">Email: somemail@mail.com</Link></li>
+            <li><Link to="#">Phone: +1234567890</Link></li>
           </ul>
         </div>
       </div>
