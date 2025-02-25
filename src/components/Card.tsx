b160c1a41c20d46c59b7f99869d8e5c947c17a04
jiwkovva19 <yankowadelyana@gmail.com>
2025-02-25 19:56:15 +0200

    subject section modified

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,10 +1,13 @@
 type CardProps ={
     image:string;
+    subject:string;
+    classes:string;
 }
-export default function Card({image}:CardProps){
+export default function Card({image, subject, classes}:CardProps){
     return(
-        <div className="p-11 bg-slate-300 rounded-lg w-[18%]">
+        <div className={`py-6 px-10 text-center bg-slate-300 rounded-lg w-[18%] ${classes}`}>
             <img className="w-[80px] h-[80px]"src={image} alt="icon" />
+            <h3 className="text-[18px] font-medium pt-2">{subject}</h3>
         </div>
     )
 
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -44,19 +44,19 @@ export default function Home() {
         />
       </div>
 
-      <div className="max-w-[900px] flex items-center ">
-        <h2></h2>
+      <div className="max-w-[900px] flex flex-col items-center ">
+        <h2 className="w-full flex items-start text-[30px] font-semibold">Popular Subjects</h2>
         <div className="flex-wrap w-full flex items-center py-5 gap-5"> 
-          <Card image={mathIcon} />
-          <Card image={mathIcon} />
-          <Card image={mathIcon} />
-          <Card image={mathIcon} />
-          <Card image={mathIcon} />
-          <Card image={mathIcon} />
-          <Card image={mathIcon} />
-          <Card image={mathIcon} />
-          <Card image={mathIcon} />
-          <Card image={mathIcon} />
+          <Card image={mathIcon} subject="Maths" classes="bg-[#FCA88F]"/>
+          <Card image={mathIcon} subject="Bulgarian" classes="bg-[#FF9F80]"/>
+          <Card image={mathIcon} subject="Geography" classes="bg-[#8BC7A5]"/>
+          <Card image={mathIcon} subject="History" classes="bg-[#D4A055]"/>
+          <Card image={mathIcon} subject="Biology" classes="bg-[#7A8755]"/>
+          <Card image={mathIcon} subject="Chemistry" classes="bg-[#EFE7DA]"/>
+          <Card image={mathIcon} subject="Physics" classes="bg-[#E9C46A]"/>
+          <Card image={mathIcon} subject="English" classes="bg-[#FFF5E1]"/>
+          <Card image={mathIcon} subject="German" classes="bg-[#D88283]"/>
+          <Card image={mathIcon} subject="IT" classes="bg-[#F8E0C6]"/>   
         </div>
       </div>
     </div>
