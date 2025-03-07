664472cfd5f631de99d0b0a7c2ef699e10086155
jiwkovva19 <yankowadelyana@gmail.com>
2025-03-07 12:09:33 +0200

    added user schemas

--- server/server.js
+++ server/server.js
@@ -1,5 +1,10 @@
 const express = require("express");
+const mongoose = require('mongoose');
 const app = express();
+
+mongoose.connect(process.env.MONGO_URI);
+
+
 
 // app.get("/api", (req, res) => {
 //   res.json({ users: ["userOne", "userTwo"] });
