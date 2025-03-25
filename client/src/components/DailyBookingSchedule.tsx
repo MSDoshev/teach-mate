5151be1e7f971df6900c600d69c4a5f6b4d0d057
jiwkovva19 <yankowadelyana@gmail.com>
2025-03-25 07:17:34 +0200

    rewriting the profile page with grid

--- client/src/components/DailyBookingSchedule.tsx
+++ client/src/components/DailyBookingSchedule.tsx
@@ -3,24 +3,24 @@ import DayOfCalendar from "./DayOfCalendar";
 export default function DailyBookingSchedule() {
   return (
     <>
-    <div className="px-[100px] max-w-[1100px]">
-    <div className="flex flex-row overflow-x-scroll gap-[10px]">
-    <DayOfCalendar/>
-    <DayOfCalendar/>
-    <DayOfCalendar/>
-    <DayOfCalendar/>
-    <DayOfCalendar/>
-    <DayOfCalendar/>
-    <DayOfCalendar/>
-    <DayOfCalendar/>
-    <DayOfCalendar/>
-    <DayOfCalendar/>
-    <DayOfCalendar/>
-    <DayOfCalendar/>
-    <DayOfCalendar/>
+      <div className="flex flex-col px-[100px] max-w-[1100px]">
 
-    </div></div>
-     
+        <div className="flex flex-row overflow-x-scroll gap-[10px]">
+          <DayOfCalendar />
+          <DayOfCalendar />
+          <DayOfCalendar />
+          <DayOfCalendar />
+          <DayOfCalendar />
+          <DayOfCalendar />
+          <DayOfCalendar />
+          <DayOfCalendar />
+          <DayOfCalendar />
+          <DayOfCalendar />
+          <DayOfCalendar />
+          <DayOfCalendar />
+          <DayOfCalendar />
+        </div>
+      </div>
     </>
   );
 }
--- client/src/pages/Profile/Profile.tsx
+++ client/src/pages/Profile/Profile.tsx
@@ -5,26 +5,30 @@ import DailyBookingSchedule from "../../components/DailyBookingSchedule";
 
 export default function TeacherProfile() {
   return (
-    <div className="px-[100px] py-[100px]">
-      <div className="flex flex-row gap-[50px]">
-        <div className="w-[200px] rounded-[100px]">
-          <img
-            className=" min-w-[200px] w-full h-[300px] object-cover"
-            src={teacherPlaceholder}
-            alt="teacher image"
-          />
+    /*main container*/
+    <div className="px-[100px] py-[100px] grid grid-cols-2 gap-[100px]">
+      {/*this is for the first column of the main container */}
+      {/*first row - image and basic info, below them is the extended biography*/}
+      <div className="grid grid-rows-2 gap-1">
+        {/*this div includes the image and the basic info in two columns*/}
+        <div className="grid grid-cols-2">
+          {/*image div */}
+          <div className="w-[200px] rounded-[100px]">
+            <img
+              className=" min-w-[200px] w-full h-[300px] object-cover"
+              src={teacherPlaceholder}
+              alt="teacher image"
+            />
+          </div>
+          {/*basic info div */}
+          <div className="gird grid-cols-3 gap-[10px]">
+            <h1 className="font-bold text-[30px]">Мария Георгиева</h1>
+            <h3 className="font-semibold text-[20px]">Учител по литература</h3>
+            <h3 className="text=[20px]">Хасково</h3>
+          </div>
         </div>
-        <div className="flex flex-col gap-[10px]">
-          <h1 className="font-bold text-[30px]">Мария Георгиева</h1>
-          <h3 className="font-semibold text-[20px]">Учител по литература</h3>
-          <h3 className="text=[20px]">Хасково</h3>
-        </div>
-        <div className="flex flex-col">
-          <DailyBookingSchedule />
-        </div>
-      </div>
-      <div className="flex flex-row">
-        <div className="w-[60%] text-[20px] font-normal">
+        {/*extended biography div*/}
+        <div>
           <h2 className="font-bold text-[25px]">За Мария</h2>
           <p>
             Мария Георгиева е опитен и вдъхновяващ учител по българска
@@ -38,12 +42,21 @@ export default function TeacherProfile() {
             учениците.
           </p>
         </div>
-
+      </div>
+      {/*second column of the main container */}
+      {/*two rows - one for the booking table and one for the map */}
+      <div className="grid grid-rows-2">
+        {/*div for the booking scjedule*/}
+        <div>
+          <DailyBookingSchedule />
+        </div>
+        {/*div for the map element*/}
         <div className="w-full h-[500px]">
           <APIProvider apiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}>
             <CustomMap />
           </APIProvider>
         </div>
+        
       </div>
     </div>
   );
